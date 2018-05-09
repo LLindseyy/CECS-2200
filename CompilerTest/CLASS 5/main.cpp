58e13b248e81fbc9ca698f065f004b8e8d220c85
Lauren <laurenlindsey1999@gmail.com>
2018-05-09 03:42:52 -1000

    BMI Homework

--- CompilerTest/CLASS 5/main.cpp
+++ CompilerTest/CLASS 5/main.cpp
@@ -16,44 +16,50 @@ int main() {
     const double
             pi=3.14159;
 
-    cout<<setprecision(8)<<"Geometry Calculator"<<endl
-        <<setprecision(10)<<"1. Calculate the area of a circle"<<endl
-        <<setprecision(10)<<"2. Calculate the area of a rectangle"<<endl
-        <<setprecision(10)<<"3. calculate the area of a triangle"<<endl
-        <<setprecision(10)<<"4. Quit"<<endl
-        <<setprecision(10)<<"enter your choice: "<<endl;
+    cout
+            <<"Geometry Calculator"<<endl
+            <<"1. Calculate the area of a circle"<<endl
+            <<"2. Calculate the area of a rectangle"<<endl
+            <<"3. calculate the area of a triangle"<<endl
+            <<"4. Quit"<<endl
+            <<"enter your choice: "<<endl;
     cin>>choice;
-    cout<<fixed<<showpoint<<setprecision(2);
-    if (choice==circle_choice) {
-        cout << "enter the radius of the circle: " << endl;
+    cout<<fixed<<showpoint;switch(choice){
+
+        case circle_choice:
+            cout << "enter the radius of the circle: " << endl;
         cin >> radius;
         area_circle=pi*pow(radius, 2);
         cout << "the area of the circle is " << area_circle << endl;
-    }
-    else if (choice==rectangle_choice)
-    {
+        break;
+
+        case rectangle_choice:
+
         cout<<"enter the length of the rectangle: "<<endl;
         cin>>length;
         cout<<"enter the width of the rectangle: "<<endl;
         cin>>width;
         area_rectangle =length * width;
         cout<<"the area of the rectangle is "<< area_rectangle<<endl;
-    }
-    else if (choice==triangle_choice)
-    {
+        break;
+
+        case triangle_choice:
+
         cout<<"enter the base of the triangle: "<<endl;
         cin>>base;
         cout<<"enter the height of the triangle: "<<endl;
         cin>>height;
         area_triangle=base*height* .5;
         cout<<"the area of the triangle is: "<<area_triangle<<endl;
-    }
-    else if (choice==quit_choice)
-    {
+        break;
+
+        case quit_choice:
+
         cout<<"program ending. "<<endl;
-    }
-    else
-    {
+        break;
+
+        default:
+
         cout<<"ERROR, the valid choices are 1-4."<<endl
             <<"Please run the program again and choose one of the valid choices"<<endl;
 
