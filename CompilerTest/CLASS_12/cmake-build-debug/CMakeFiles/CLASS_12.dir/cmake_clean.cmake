fa66e35e004c8d1255847967de8ba028e04611e2
Lauren <laurenlindsey1999@gmail.com>
2018-05-28 17:19:55 -0400

    pokemon
    ciclo for

---
+++ CompilerTest/CLASS_12/cmake-build-debug/CMakeFiles/CLASS_12.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLASS_12.dir/main.cpp.o"
+  "CLASS_12.pdb"
+  "CLASS_12"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLASS_12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
